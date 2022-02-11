6ebe465c8820fd793c233e8c1dcaac8e48d32f39
AntonioPavkovic <antoniopavkovic999@gmail.com>
2022-02-11 16:49:37 +0100

    ran npm run build in client folder and made slight changes on the frontend

--- client/src/components/views/NavBar/NavBar.js
+++ client/src/components/views/NavBar/NavBar.js
@@ -4,6 +4,8 @@ import RightMenu from "./Sections/RightMenu";
 import { Drawer, Button, Icon } from "antd";
 import "./Sections/Navbar.css";
 
+const Logo = require('../../../assets/images/Logo.png');
+
 function NavBar() {
   const [visible, setVisible] = useState(false);
 
@@ -21,7 +23,7 @@ function NavBar() {
       style={{ position: "fixed", zIndex: 5, width: "100%" }}
     >
       <div className="menu__logo">
-        <a href="/">MooV</a>
+        <a href="/"><img src={Logo} alt="Logo" style={{ width: '100%', marginTop: '-5px' }} /></a>
       </div>
       <div className="menu__container">
         <div className="menu_left">
