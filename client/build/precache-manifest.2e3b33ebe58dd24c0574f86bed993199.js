883ae7443ba4ce03ed8a61afc003c47fb7939859
AntonioPavkovic <antoniopavkovic999@gmail.com>
2022-02-15 15:19:49 +0100

    removed build folder form .gitignore

---
+++ client/build/precache-manifest.2e3b33ebe58dd24c0574f86bed993199.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a7e26fb16359c4bb1de8be673d342f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "183e3bbf62ddddf12f59",
+    "url": "/static/css/2.48db8b0f.chunk.css"
+  },
+  {
+    "revision": "c5982225a56b6938d020",
+    "url": "/static/css/main.db0ebcd3.chunk.css"
+  },
+  {
+    "revision": "183e3bbf62ddddf12f59",
+    "url": "/static/js/2.449b7312.chunk.js"
+  },
+  {
+    "revision": "0f87e0cfbb145332e9889452e79883f4",
+    "url": "/static/js/2.449b7312.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5982225a56b6938d020",
+    "url": "/static/js/main.d45ddbb8.chunk.js"
+  },
+  {
+    "revision": "65762bcbee126357b070",
+    "url": "/static/js/runtime-main.35eb2b37.js"
+  },
+  {
+    "revision": "0e4c6b31feadee72442de6770cb9ed67",
+    "url": "/static/media/Logo.0e4c6b31.png"
+  },
+  {
+    "revision": "1caf87b920863db3d82a53a41ad93fde",
+    "url": "/static/media/antonio.1caf87b9.jpeg"
+  },
+  {
+    "revision": "7cc43f4e65af801d08e8dfe041d11d3e",
+    "url": "/static/media/nikola.7cc43f4e.jpeg"
+  }
+]);
